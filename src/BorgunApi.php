09e14acc54729dbaf857af9eca206c129e0c5a94
vadgab <gabor.vadasz.allstar.hu>
2022-11-23 17:06:57 +0100

    v0.1

--- src/BorgunApi.php
+++ src/BorgunApi.php
@@ -22,12 +22,15 @@ class BorgunApi
     public $language = "";
     public $buyername = "";
     public $buyeremail = "";
-    public $items = array();
+    private $items = array();
+    public $item = array();
     public $amount = "";
     public $pagetype = "";
     public $skipreceiptpage = "";
     public $merchantlogo = "https://www.b-payment.hu/docs/images/logo.jpg";
     public $merchantemail = "";
+
+    private $postData = array();
 
     const URL_MAIN_TEST = "https://www.szamlazz.hu/szamla/";
     const URL_MAIN_LIVE = "https://www.szamlazz.hu/szamla/";
@@ -39,6 +42,43 @@ class BorgunApi
 
     public function sendPayment(){
 
+        /* MAIN DATA */
+        $this->post_data['merchantid'] = $this->merchantid;
+        $this->post_data['paymentgatewayid'] = $this->paymentgatewayid;
+        $this->post_data['checkhash'] = $this->checkhash;
+        $this->post_data['orderid'] = $this->orderid;
+        $this->post_data['currency'] = $this->currency;
+        $this->post_data['language'] = $this->language;
+        $this->post_data['buyername'] = $this->buyername;
+        $this->post_data['buyeremail'] = $this->buyeremail;
+        $this->post_data['amount'] = $this->amount;
+        $this->post_data['merchantid'] = $this->merchantid;
+
+        /* items add post data */
+        $count = 0;
+        if(!empty($this->items))foreach($this->items as $items){
+            foreach($items as $key => $items_add){
+                $this->post_data['item'.$key.'_'.$count] = $items_add;
+            }
+        }
+
+        /*   return URL */
+        $this->post_data['returnurlsuccess'] = Yii::$app->params['base_url'].str_replace("{bPaymentModules}",$this->bPaymentModules,self::RETURN_SUCCESS);
+        $this->post_data['returnurlcancel'] = Yii::$app->params['base_url'].str_replace("{bPaymentModules}",$this->bPaymentModules,self::RETURN_CANCEL);
+        $this->post_data['returnurlerror'] = Yii::$app->params['base_url'].str_replace("{bPaymentModules}",$this->bPaymentModules,self::RETURN_ERROR);
+
+
+    }
+
+    public function addItem(){
+
+        /* MAIN DATA */
+        $this->item['description'] = '';
+        $this->item['count'] = '';
+        $this->item['unitamount'] = '';
+        $this->item['amount'] = '';
+        /* add items schema */
+        $this->items[] = $this->item;
 
     }
 
